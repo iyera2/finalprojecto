34ade6fcac2ecfb63acb71db02721f2e697e0cdc
Arjun Iyer <iyera2@s.dcsdk12.org>
2019-12-16 20:44:40 -0700

    Last commit

--- fp.js
+++ fp.js
@@ -1,21 +1,98 @@
 function ready(){
     console.log("Page Ready");
+    myGood.onclick = function(){
+            i = 0;
+            if (i === 0){
+                var x = document.createElement("AUDIO"); // creates a new element to insert into page
 
-    mySelect.onclick () {
-        myFunction()
-        let x = document.getElementById("happy"); 
-        let y = document.getElementById("mySelect").value;
-        document.getElementById("demo").innerHTML = "You selected: " + y;
-        if (y === "Happy") {
-            playHappy() 
-                x.play(); 
-            };
+                if (x.canPlayType("audio/mpeg")) {
+                    x.setAttribute("src","happy.mp3"); // this is where the audio file goes
+              
+                x.setAttribute("autoplay", "autoplay"); //autoplays it, yes there is no pause button
+                document.body.appendChild(x); // adds the  music to the document
+            myImg.onclick = function(){ // created an onclick for the image so everytime it is onclick the image will change
+                document.getElementById("myImg").src = "happy.gif";
+        }
+        
+    }
+        while (i < 1){
+            i = i + 1
+        }
+            if (i === 1){
+                myLess.onclick = function(){
+                    var x = document.createElement("AUDIO");
+            
+                    if (x.canPlayType("audio/mpeg")) {
+                        x.setAttribute("src","drake.mp3");
+                     
+                    x.setAttribute("autoplay", "autoplay");
+                    document.body.appendChild(x);
+               myImg.onclick = function(){
+                   document.getElementById("myImg").src = "drake.gif";
+               }   
+            }
+                 }
+            }
+             
         };
+    } // repeition from here to down there
 
-    good.onclick = function playHappy() {}
-       
+      mySad.onclick = function(){
+         var x = document.createElement("AUDIO");
 
-    
+             if (x.canPlayType("audio/mpeg")) {
+                 x.setAttribute("src","whenwe.mp3");
+              
+             x.setAttribute("autoplay", "autoplay");
+             document.body.appendChild(x);
+        myImg.onclick = function(){
+            document.getElementById("myImg").src = "adell.gif";
+        }
+             
+         };
+     }
+
+     myAngry.onclick = function(){
+        var x = document.createElement("AUDIO");
+
+        if (x.canPlayType("audio/mpeg")) {
+            x.setAttribute("src","don'town.mp3");
+         
+        x.setAttribute("autoplay", "autoplay");
+        document.body.appendChild(x);
+   myImg.onclick = function(){
+       document.getElementById("myImg").src = "random.gif";
+   }   
+}
+     }
+
+     myDeep.onclick = function(){
+        var x = document.createElement("AUDIO");
+
+        if (x.canPlayType("audio/mpeg")) {
+            x.setAttribute("src","ben.mp3");
+         
+        x.setAttribute("autoplay", "autoplay");
+        document.body.appendChild(x);
+   myImg.onclick = function(){
+       document.getElementById("myImg").src = "ben.gif";
+   }   
+}
+     }
+     myLess.onclick = function(){
+        var x = document.createElement("AUDIO");
+
+        if (x.canPlayType("audio/mpeg")) {
+            x.setAttribute("src","drake.mp3");
+         
+        x.setAttribute("autoplay", "autoplay");
+        document.body.appendChild(x);
+   myImg.onclick = function(){
+       document.getElementById("myImg").src = "drake.gif";
+   }   
+}
+     }
+}
 
 
-document.addEventListener("DOMContentLoaded", ready);
+document.addEventListener("DOMContentLoaded", ready); //gets the page ready to play
